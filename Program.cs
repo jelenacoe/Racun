6c4ecf87d65374435c25114b214c1d9714a143da
jelenacoe <58305766+jelenacoe@users.noreply.github.com>
2020-07-13 14:41:16 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Racun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Racun r1 = new Racun("123123123", "Jovan Jovic", 1000, -200);
+
+            Console.WriteLine("Trenutno stanje na racunu je {0}", r1.SredstvaNaRacunu);
+            r1.DodajPareNaRacun(200);
+            Console.WriteLine("Trenutno stanje na racunu je {0}", r1.SredstvaNaRacunu);
+
+            if (r1.SkiniPareSaRacuna(1000))
+                Console.WriteLine("Trenutno stanje na racunu je {0}", r1.SredstvaNaRacunu);
+            else
+                Console.WriteLine("Nije moguce skinuti pare sa racuna");
+
+
+            if (r1.SkiniPareSaRacuna(250))
+                Console.WriteLine("Trenutno stanje na racunu je {0}", r1.SredstvaNaRacunu);
+            else
+                Console.WriteLine("Nije moguce skinuti pare sa racuna");
+
+
+            if (r1.SkiniPareSaRacuna(10000))
+                Console.WriteLine("Trenutno stanje na racunu je {0}", r1.SredstvaNaRacunu);
+            else
+                Console.WriteLine("Nije moguce skinuti pare sa racuna");
+        }
+    }
+}
---
+++ Racun.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Racun
+{
+    class Racun
+    {
+        private string brojRacuna;
+        private string imeKorisnika;
+        private double sredstvaNaRacunu;
+        private double dozvoljeniMinus;
+
+        public Racun(string brojRacuna, string imeKorisnika, double sredstvaNaRacunu, double dozvoljeniMinus)
+        {
+            this.brojRacuna = brojRacuna;
+            this.imeKorisnika = imeKorisnika;
+            this.sredstvaNaRacunu = sredstvaNaRacunu;
+            this.dozvoljeniMinus = dozvoljeniMinus;
+        }
+
+        public string BrojRacuna
+        {
+            get
+            {
+                return brojRacuna;
+            }
+            
+        }
+
+        public string ImeKorisnika
+        {
+            get
+            {
+                return imeKorisnika;
+            }
+            
+        }
+
+        public double SredstvaNaRacunu
+        {
+            get
+            {
+                return sredstvaNaRacunu;
+            }
+        }
+
+        public  bool SkiniPareSaRacuna(double suma)
+        {
+            if (SredstvaNaRacunu - suma > dozvoljeniMinus)
+            {
+                sredstvaNaRacunu -= suma;
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+            
+        }
+
+        public void DodajPareNaRacun(double suma)
+        {
+            sredstvaNaRacunu += suma;
+        }
+    }
+}
